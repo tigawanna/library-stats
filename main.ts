5fb3a97b84dbae40391a548a75c98c571f8e5255
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-01-02 10:02:43 +0300

    top level listenQueue

--- main.ts
+++ main.ts
@@ -9,6 +9,13 @@ import { load } from "https://deno.land/std@0.210.0/dotenv/mod.ts";
 
 const app = new Hono();
 
+const db = await Deno.openKv();
+db.listenQueue(async (msg) => {
+  const data = msg as { channel: string; text: string };
+  console.log("=== QUEUE MESSAGE === ", data);
+  await db.set(["test-queues", data.channel], data.text);
+});
+
 app.use("*", logger(), poweredBy());
 app.get("/", (c) => {
   return c.text("Hello Deno!");
@@ -21,16 +28,17 @@ app.get("/stats/fresh_compute", (c) => {
   return getFreshComputeRoute(c);
 });
 
+
 app.get('/test', async(c) => {
   try {
     const env = await load();
     const db = await Deno.openKv();
 
-    db.listenQueue(async(msg) => {
-      const data = msg as { channel: string; text: string };
-      console.log("=== QUEUE MESSAGE === ",data);
-      await db.set(["test-queues", data.channel], data.text);
-    });
+    // db.listenQueue(async(msg) => {
+    //   const data = msg as { channel: string; text: string };
+    //   console.log("=== QUEUE MESSAGE === ",data);
+    //   await db.set(["test-queues", data.channel], data.text);
+    // });
 
     await db.enqueue({ channel: "C123456", text: "Slack message" }, {
       delay: 0,
