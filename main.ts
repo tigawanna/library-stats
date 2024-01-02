1dcad12a7f873ca924dd30dd78693d936d6f8cc3
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2024-01-02 10:13:47 +0300

    top level

--- main.ts
+++ main.ts
@@ -9,12 +9,7 @@ import { load } from "https://deno.land/std@0.210.0/dotenv/mod.ts";
 
 const app = new Hono();
 
-const db = await Deno.openKv();
-db.listenQueue(async (msg) => {
-  const data = msg as { channel: string; text: string };
-  console.log("=== QUEUE MESSAGE === ", data);
-  await db.set(["test-queues", data.channel], data.text);
-});
+
 
 app.use("*", logger(), poweredBy());
 app.get("/", (c) => {
@@ -29,10 +24,18 @@ app.get("/stats/fresh_compute", (c) => {
 });
 
 
+const db = await Deno.openKv();
+db.listenQueue(async (msg) => {
+  const data = msg as { channel: string; text: string };
+  console.log("=== QUEUE MESSAGE === ", data);
+  await db.set(["test-queues", data.channel], data.text);
+  console.log("saved to kv ==== ",await db.get(["test-queues", data.channel]));
+});
+
 app.get('/test', async(c) => {
   try {
     const env = await load();
-    const db = await Deno.openKv();
+    // const db = await Deno.openKv();
 
     // db.listenQueue(async(msg) => {
     //   const data = msg as { channel: string; text: string };
